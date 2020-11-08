1890680153d3c7c0db623f1b5abe0738336951df
steven cao <stevencao23@gmail.com>
2020-11-08 21:03:48 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,6 @@
+def meal_choice (protein = "tofu", veg1, veg2)
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+meal_choice "carrots", "string beans", "lentils"
